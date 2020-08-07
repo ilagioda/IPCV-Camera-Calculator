f1251650f493f8da23126c88137e19b717aa6855
Paolo Gallo <45040214+paolo257428@users.noreply.github.com>
2020-08-07 18:08:50 +0200

    Ulteriori prove per video multithread

--- prove/multithread video.py
+++ prove/multithread video.py
@@ -1,42 +1,34 @@
 from threading import Thread
-from queue import Queue
-import random
+import queue
 import sys
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
 
-class VideoStreamReader:
+class VideoPlayer:
     """
-    Class that continuously gets frames from a VideoCapture object
-    with a dedicated thread and accumulates them in a queue.
+    Class that shows frames from a video sequence through cv.imshow()
+    with a dedicated thread, by picking them from a queue.
     """
 
-    def __init__(self, srctype, path, queueSize=1):
+    def __init__(self, framerate=30, queueSize=30):
         """
-        Initialize the VideoCapture stream and the frame buffer
+        Initialization
         """
-        self.stream = cv2.VideoCapture(path)
         self.stopped = False
-        self.queue = Queue(maxsize=queueSize)
+        self.rate = framerate
+        self.queue = queue.Queue(maxsize=queueSize)
 
-        if srctype == 'webcam':
-            # Webcam
-            self.rate = 30
-        elif srctype == 'video':
-            # Video file
-            self.rate = self.stream.get(cv2.CAP_PROP_FPS)
 
     def start(self):
         """
-        Start a thread to read frames from the video stream
+        Start a thread to show frames from the video buffer
         """
-        t = Thread(target=self.update, args=())
-        t.daemon = True
+        t = Thread(target=self.update, args=(), daemon=True)
         t.start()
         return self
+
 
     def update(self):
         """
@@ -44,65 +36,71 @@ class VideoStreamReader:
         """
         while not self.stopped:
 
-            # If the queue is not full...
-            if not self.queue.full():
-                # ...read the next frame from the video stream
-                (grabbed, frame) = self.stream.read()
+            # If the queue is not empty
+            if not self.queue.empty():
+                # Get the next frame from the video buffer
+                frame = self.queue.get()
 
-                # Check if we have reached the end of the video file
-                # or if the webcam has been disconnected
-                if not grabbed:
-                    self.stop()
-                    return
+                # And show it on the screen
+                cv2.imshow('Video', frame)
 
-                # Add the new frame to the queue
-                self.queue.put(frame)
+                # Wait for the proper time (int(1000/self.rate))
+                # and also check if the user has pressed the ESC key to quit
+                if cv2.waitKey(int(1000/self.rate)) & 0xFF == ord('\x1b'):
+                    self.stopped = True
+                    break
 
-            else:
-                # Avoid useless looping, pause for the time needed to consume 1 frame
-                time.sleep(1/self.rate)
+                # Check if the window has been closed by the user
+                if cv2.getWindowProperty('Video', cv2.WND_PROP_VISIBLE) < 1:
+                    self.stopped = True
+                    break
 
-    def stop(self):
+
+    def show(self, frame):
         """
-        Sets the VideoStreamReader `stopped` flag
+        Push the next frame in the queue (blocking operation if queue is full)
         """
-        self.stopped = True
+        done = False
+        while not done and not self.stopped:
+            try:
+                self.queue.put(frame, timeout=1)
+                done = True
+            except queue.Full:
+                done = False
 
-    def read(self):
-        """
-        Get the next frame in the queue (blocking operation if queue is empty)
-        """
-        return self.queue.get()
-        #(grabbed, frame) = self.stream.read()
-        #if not grabbed:
-        #    self.stop()
-        #return frame
 
     def finished(self):
         """
-        Returns false as long as there are frames that need to be processed
+        Returns false as long as the video player is running
         """
-        return self.stopped and self.queue.qsize() == 0
+        return self.stopped
 
 
-def thread_video_test():
+def grab_frame(cap):
     """
-    Dedicated thread for grabbing video frames with VideoStreamReader object.
-    Main thread shows video frames.
+    Method to grab a frame from the camera
+    :param cap: the VideoCapture object
+    :return: the captured image
     """
+    _, frame = cap.read()
 
-    # Initialize the random number generator
-    random.seed(12345)
+    if frame is not None:
+        height = frame.shape[0]
+        width = frame.shape[1]
 
-    video_getter = VideoStreamReader('video', "C:\\Users\\JackaL\\source\\repos\\IPCV-Camera-Calculator\\video\\matita 17+65.mp4").start()
+        # Resize the frame to 960x540 (preserve aspect ratio)
+        factor = min(960 / width, 540 / height)
+        new_size = (int(width * factor), int(height * factor))
+        frame = cv2.resize(frame, new_size, interpolation=cv2.INTER_CUBIC)
 
-    # Set up MatPlotLib plot to draw frames in interactive mode
-    plt.ion()
+    return frame
 
-    # Create a figure to be updated
-    fig = plt.figure()
-    fig.canvas.mpl_connect("close_event", lambda event: video_getter.stop())
-    ax_img = None
+
+if __name__ == "__main__":
+
+    # Initialize video objects
+    cap = cv2.VideoCapture("E:\\DOWNLOAD\\16+40.mp4")
+    video_shower = VideoPlayer().start()
 
     # Timer parameters
     cont = 0
@@ -110,67 +108,50 @@ def thread_video_test():
 
     t0 = time.time()
 
-    while not video_getter.finished():
+    while cap.isOpened():
 
-        frame = video_getter.read()
-        if frame is not None:
-            height = frame.shape[0]
-            width = frame.shape[1]
-
-            # Resize the frame to 960x540 (preserve aspect ratio)
-            factor = min(960 / width, 540 / height)
-            new_size = (int(width * factor), int(height * factor))
-            frame = cv2.resize(frame, new_size, interpolation=cv2.INTER_CUBIC)
-
-            # Print cont
-            print("Cont: {}".format(cont))
-
-            # Check if the tracked yellow object has exited the video for enough frames
-            if cont == stop_cont:
-                print("Done")
-
-            # Convert the current frame in HSV (note: needed by cv.inRange())
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # Thresholding with the usage of a mask for detecting the yellow
-            lower_yellow = np.array([20, 110, 110])
-            upper_yellow = np.array([40, 255, 255])
-            mask = cv2.inRange(img, lower_yellow, upper_yellow)
-
-            # Find contours of yellow objects
-            (contours, _) = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            flag = 0
-            for contour in contours:
-                flag = 1
-                area = cv2.contourArea(contour)
-                if area > 800:
-                    # Yellow object found
-                    cont = 0
-                    x, y, w, h = cv2.boundingRect(contour)
-                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 10)
-            if flag == 0:
-                # Yellow object not found
-                cont += 1
-
-            if ax_img is None:
-                ax_img = plt.imshow(frame)
-                plt.axis("off")  # hide axis, ticks, ...
-                plt.title("Buffered VideoCapture")
-                plt.show()
-            else:
-                ax_img.set_data(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-                fig.canvas.draw()
-
-        else:
+        # Get the current frame
+        frame = grab_frame(cap)
+        if frame is None:
             break
-    
+
+        if video_shower.finished():
+            cap.release()
+            break
+
+        # Print cont
+        print("Cont: {}".format(cont))
+
+        # Check if the tracked yellow object has exited the video for enough frames
+        if cont == stop_cont:
+            print("Done")
+
+        # Convert the current frame in HSV (note: needed by cv.inRange())
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Thresholding with the usage of a mask for detecting the yellow
+        lower_yellow = np.array([20, 110, 110])
+        upper_yellow = np.array([40, 255, 255])
+        mask = cv2.inRange(img, lower_yellow, upper_yellow)
+
+        # Find contours of yellow objects
+        (contours, _) = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        flag = 0
+        for contour in contours:
+            flag = 1
+            area = cv2.contourArea(contour)
+            if area > 800:
+                # Yellow object found
+                cont = 0
+                x, y, w, h = cv2.boundingRect(contour)
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 10)
+        if flag == 0:
+            # Yellow object not found
+            cont += 1
+
+        # Enqueue the frame for showing it on the screen
+        video_shower.show(frame)
+
     t1 = time.time() - t0
     print("Time elapsed: ", t1)
-
-
-if __name__ == "__main__":
-    try:
-        thread_video_test()
-    except KeyboardInterrupt:
-        sys.exit(0)
     
