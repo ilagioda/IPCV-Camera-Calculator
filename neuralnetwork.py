6be6db89d5ba57a9faff98a6062178a6dfccc69f
Paolo Gallo <45040214+paolo257428@users.noreply.github.com>
2020-08-08 11:22:32 +0200

    Refactoring minori del modulo neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,6 +1,10 @@
+"""
+Neural Network-based classifier, used to predict the arithmetical symbols
+(digits and operators) from cropped images that contain a single symbol each
+"""
+
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 import utils
 
 import torch
@@ -10,24 +14,30 @@ from torchvision.models import alexnet
 
 from PIL import Image
 
+
+# Set of 'global' classifier variables
 NUM_CLASSES = 16
 NN_PATH = './preproc_augm_symbols_net.pth'
-LABELS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'div', 'equal', 'minus', 'mul', 'plus', 'separator']
+LABELS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+          'div', 'equal', 'minus', 'mul', 'plus', 'separator']
+
 
 def prepare_image(image):
     """
     Image pre-processing, before running them through the neural network
     :param image: an RGB image of a symbol that has to be processed by the neural network
-    :return: preparred image
+    :return: the prepared and pre-processed image
     """
     if image is None:
-        return
+        return None
 
     # Convert to grayscale
-    img_gray = utils.rgb_to_gray(image)
+    img_proc = utils.rgb_to_gray(image)
 
     # Apply thresholding to the image, separating the background from the symbol
-    thresh = cv.adaptiveThreshold(img_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, 2)
+    img_proc = cv.adaptiveThreshold(img_proc, 255,
+                                    cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                    cv.THRESH_BINARY, 15, 2)
 
     # Define a kernel for morphological operations
     kernel = np.array(
@@ -37,7 +47,7 @@ def prepare_image(image):
         np.uint8)
 
     # Apply closing operator to remove noise
-    thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+    img_proc = cv.morphologyEx(img_proc, cv.MORPH_CLOSE, kernel)
 
     # Fit the image in a squared area with a 8px margin on each side (without resizing)
     height = image.shape[0]
@@ -53,30 +63,31 @@ def prepare_image(image):
     x_start = int((size-width)/2)
     y_end = y_start + height
     x_end = x_start + width
-    image_prepared[y_start:y_end, x_start:x_end] = thresh
+    image_prepared[y_start:y_end, x_start:x_end] = img_proc
 
     return image_prepared
 
 
-def predict_symbol(img_cv):
+def predict_symbol(image_cv):
     """
     Given a certain image containing a symbol, allows to predict the class label, i.e.
-    the category to which the symbol belongs, through the usage of a pre-trained neural network (AlexNet)
+    the type of symbol, through the usage of a pre-trained neural network (AlexNet)
     :param img: an image containing a symbol
     :return: predicted label, i.e. predicted symbol
     """
 
-    # Convert into PIL format
-    img_cv = cv.cvtColor(img_cv, cv.COLOR_BGR2RGB)
-    img = Image.fromarray(img_cv).convert("RGB")
+    # Convert image into PIL format
+    image_cv = cv.cvtColor(image_cv, cv.COLOR_BGR2RGB)
+    image = Image.fromarray(image_cv).convert("RGB")
 
     # Define transforms for the prediction phase
-    eval_transform = transforms.Compose([transforms.Resize(224),                                 # Resizes short size of the PIL image to 256
-                                         transforms.ToTensor(),                                  # Turn PIL Image to torch.Tensor
-                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizes tensor with mean and standard deviation
-                                         ])
+    eval_transform = transforms.Compose(
+        [transforms.Resize(224),                                    # Resizes short size of the PIL image to 256
+         transforms.ToTensor(),                                     # Turn PIL Image to torch.Tensor
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))     # Normalizes tensor with mean and standard deviation
+        ])
 
-    # Define a new model
+    # Define a new NN model
     net = alexnet()
 
     # Change the last layer of AlexNet to output 16 classes
@@ -89,11 +100,11 @@ def predict_symbol(img_cv):
     net.eval()
 
     # Prepare the image for the network
-    img = eval_transform(img).float()
-    img = img.unsqueeze(0)
+    image = eval_transform(image).float()
+    image = image.unsqueeze(0)
 
     # Forward the image through the network
-    outputs = net(img)
+    outputs = net(image)
 
     # Get prediction (index in the LABELS array)
     pred = torch.argmax(outputs)
